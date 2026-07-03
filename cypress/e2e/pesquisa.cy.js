d56455be0d9e27135a243fca5a1495d35a94c7a3
Júlia Goulart Cunha <juliagoulartcunha@alunos.utfpr.edu.br>
2026-07-03 20:35:35 -0300

    após correções

--- cypress/e2e/pesquisa.cy.js
+++ cypress/e2e/pesquisa.cy.js
@@ -32,6 +32,34 @@ describe('Wikipedia', () => {
 
   })
 
+  it('Validar a URL após a pesquisa', () => {
+
+    const pesquisa = 'Sabrina Carpenter'
+
+    cy.visit('https://www.wikipedia.org/')
+    cy.get('#searchInput').type(pesquisa)
+    cy.get('.pure-button').click()
+
+    cy.origin('https://pt.wikipedia.org', () => {
+      cy.url().should('include', 'Sabrina_Carpenter')
+    })
+
+  })
+
+  it('Validar que existe uma imagem principal no artigo', () => {
+
+    const pesquisa = 'Sabrina Carpenter'
+
+    cy.visit('https://www.wikipedia.org/')
+    cy.get('#searchInput').type(pesquisa)
+    cy.get('.pure-button').click()
+
+    cy.origin('https://pt.wikipedia.org', () => {
+      cy.get('.infobox img').should('be.visible')
+    })
+
+  })
+
   it('Pesquisar um termo inexistente', () => {
 
     const termoinex = 'djfn6d5f'
@@ -56,5 +84,35 @@ describe('Wikipedia', () => {
     })
 
   })
+
+it('Pesquisar novamente após uma pesquisa sem resultados', () => {
+
+  const pesquisa2 = 'Ariana Grande'
+
+  cy.visit('https://www.wikipedia.org/')
+
+  cy.get('#searchInput').type('djfn6d5f')
+  cy.get('.pure-button').click()
+
+  cy.origin(
+    'https://pt.wikipedia.org',
+    { args: { pesquisa2 } },
+    ({ pesquisa2 }) => {
+
+      cy.get('.mw-search-nonefound')
+        .should('contain', 'A pesquisa não produziu resultados.')
+
+      cy.get('#ooui-php-1')
+        .clear()
+        .type(pesquisa2)
+
+      cy.get('.oo-ui-buttonElement-button').first().click()
+
+      cy.contains('Ariana Grande').should('be.visible')
+
+    }
+  )
+
+})
 
 })
